3755394b967aefce1976e91b47d9273ed8e33672
skating <tangtianyu114514@outlook.com>
2025-08-08 08:07:38 +0800

    Add files via upload

--- scan_xui.go
+++ scan_xui.go
@@ -17,8 +17,10 @@ import (
 
 // XUI_SIGNATURES 存放了用于识别 x-ui 面板的特征字符串
 var XUI_SIGNATURES = []string{
-	`src="/assets/js/model/xray.js"`,
-	`href="/assets/ant-design-vue"`,
+	`src="/assets/js/model/xray.js`,
+	`href="/assets/ant-design-vue`,
+	`location.href = basePath + 'panel/'`,
+	`location.href = basePath + 'xui/'`,
 }
 
 // AppConfig 结构体用于存储应用程序的配置
